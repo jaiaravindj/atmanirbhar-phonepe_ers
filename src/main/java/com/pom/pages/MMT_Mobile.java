4de660d30cefc358b6fea50bb0962c1172dc54ba
Jai Aravind J <aravindshadow@gmail.com>
2020-05-30 16:21:21 +0530

    Add login Page Action and step

--- src/main/java/com/pom/pages/MMT_Mobile.java
+++ src/main/java/com/pom/pages/MMT_Mobile.java
@@ -1,8 +1,11 @@
 package com.pom.pages;
 
+import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import com.pom.framework.TestBase;
 import com.pom.utilities.Logs;
@@ -14,20 +17,55 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
 
-
 public class MMT_Mobile extends TestBase {
-	
-	
-	public MMT_Mobile() {
+    private String testEmail = "atmanirbhar.phonep.ers@gmail.com";
+    private String password = "Sumed@Jai123";
+    TestBase testBase = new TestBase();
 
-		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-		Logs.INFO("This is constructor - MMT");
-	}
-	
-	
-	
+    private MobileElement gLoginDismiss() {
+        return driver.findElementById("com.google.android.gms:id/cancel");
+    }
 
- 	
-	
+    private MobileElement loginEmailText() {
+        return driver.findElementByClassName("android.widget.EditText");
+    }
+
+    private MobileElement loginContinueButton() {
+        return driver.findElementById("com.makemytrip:id/btn_continue");
+    }
+
+    private MobileElement loginViaPasswordButton() {
+        return driver.findElementById("com.makemytrip:id/pwd_option");
+    }
+
+    private MobileElement mmtUniversalSearchLayout(){
+        return driver.findElementById("com.makemytrip:id/universal_search");
+    }
+
+
+    public MMT_Mobile() {
+
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        Logs.INFO("This is constructor - MMT");
+    }
+
+    public void MMT_Login() {
+
+        testBase.waitForElement(gLoginDismiss(),3);
+        testBase.click(gLoginDismiss());
+
+        testBase.enterText(loginEmailText(), testEmail);
+        testBase.click(loginContinueButton());
+
+        testBase.click(loginViaPasswordButton());
+        testBase.enterText(loginEmailText(), password);
+        testBase.click(loginContinueButton());
+        testbase.waitForElement(mmtUniversalSearchLayout(),3);
+    }
+
+    public void MMT_Hotels() {
+        testbase.click(mmtUniversalSearchLayout());
+    }
+
 
 }
